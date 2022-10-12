1b32927fb49e9f64fc13712ebebcb4e7e9d81eb4
David Ferreira <david.regatia@gmail.com>
2022-10-12 22:27:26 +0100

    Organizes script and refactors naming convention

--- scripts/createVersionsFile.py
+++ scripts/createVersionsFile.py
@@ -4,74 +4,79 @@ import re
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 load_dotenv("filePaths.env")
 
-GITHUB_REPOS_FILE = os.getenv('REPOS-REGISTRY')
-OUTPUT_FILE = os.getenv('PROJECT-VERSIONS-FILE')
+GITHUB_ACCESS_TOKEN = os.getenv('GITHUB-ACCESS-TOKEN')
+INPUT_FILE = os.getenv('REPOS-REGISTRY')
+OUTPUT_FILE = os.getenv('REPOS-VERSIONS-FILE')
 NUMBER_PROJECTS=200
 
 s = requests.Session()
-headers = {'Authorization': 'token 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'}
+headers = {'Authorization': 'token ' + str(GITHUB_ACCESS_TOKEN)}
 
-def get_versions(repo):
-	orig_repo = repo 
-	repo = repo + "releases"
-	print(repo, end=">")
-	data = s.get(f"https://api.github.com/repos/{repo}", headers=headers)
-	resultado = data.json()
-
-	if len(resultado) == 0:
-		return get_tags(orig_repo)
-
-	first_version =  resultado[0]["tag_name"]
-	last_version = resultado[len(resultado) -1]["tag_name"]
-	print(f"{last_version}, {first_version}")
-	return last_version, first_version
-
-def get_tags(repo):
-	repo = repo + "tags"
-	print( repo, end=">")
-	data = s.get(f"https://api.github.com/repos/{repo}", headers=headers)
-	resultado = data.json()
-
-	if len(resultado) == 0:
-		return "None", "None"
-
-	first_version =  resultado[0]["name"]
-	last_version = resultado[len(resultado) -1]["name"]
-	print(f"{last_version}, {first_version}")
-	return last_version, first_version
-
-
-
-def main():
-
-    source_file = open(GITHUB_REPOS_FILE)
+def readURLsFromInputFile():
+    sourceFile = open(INPUT_FILE)
     links = []
-    for line in source_file.readlines():
+    for line in sourceFile.readlines():
         x = line.split("/")
         links.append(f"{x[-2]}/{x[-1].strip()}/\n")
+    sourceFile.close()
+    return links
 
-    top_links = links[:NUMBER_PROJECTS]
+def limitNumberOfProjects(links):
     print(f"Processing top {NUMBER_PROJECTS} projects...")
-    source_file.close()
-    out_file = open(OUTPUT_FILE, 'w+')
-    for line in top_links:
-        stripped_line = line.strip()
-        last, first = get_versions(stripped_line)
+    return links[:NUMBER_PROJECTS]
 
-        # add the github link to each line and remove '/releases'
-        patt = re.compile('(\s*)/releases(\s*)')
-        link = patt.sub('\\1\\2', stripped_line)
-        stripped_line = f"https://github.com/{link}"
+def getVersions(repo):
+    originRepo = repo 
+    repo = repo + "releases"
+    print(repo, end=">")
+    versions = s.get(f"https://api.github.com/repos/{repo}", headers=headers)
+    result = versions.json()
+    try:
+        projectFirstVersion = result[0]["tag_name"]
+        projectLastVersion = result[len(result) -1]["tag_name"]
+        print(f"{projectLastVersion}, {projectFirstVersion}")
+        return [projectLastVersion, projectFirstVersion]
+    except:
+        return getTags(originRepo)
 
-        out_file.write(f"{stripped_line};{last} \n")
-        time.sleep(1)
-        out_file.write(f"{stripped_line};{first} \n")
-        time.sleep(1)
+def getTags(repo):
+    repo = repo + "tags"
+    print( repo, end=">")
+    tags = s.get(f"https://api.github.com/repos/{repo}", headers=headers)
+    result = tags.json()
+    try:
+        projectFirstVersion =  result[0]["name"]
+        projectLastVersion = result[len(result) -1]["name"]
+        print(f"{projectLastVersion}, {projectFirstVersion}")
+        return [projectLastVersion, projectFirstVersion]
+    except:
+        return ["None", "None"]
+    
+def createRepoVersionURLString(strippedLine):
+    patt = re.compile('(\s*)/releases(\s*)')
+    link = patt.sub('\\1\\2', strippedLine)
+    return f"https://github.com/{link}"
 
+def saveRepoVersionURLToOutput(outputFile, strippedLine, versions):
+    outputFile.write(f"{strippedLine};{versions[1]} \n")
+    time.sleep(1)
+    outputFile.write(f"{strippedLine};{versions[0]} \n")
+    time.sleep(1)  
+
+def main():
+    links = readURLsFromInputFile()
+    topLinks = limitNumberOfProjects(links)
+    outputFile = open(OUTPUT_FILE, 'w+')
+    for line in topLinks:
+        strippedLine = line.strip()
+        versions = getVersions(strippedLine)
+        strippedLine = createRepoVersionURLString(strippedLine)
+        saveRepoVersionURLToOutput(outputFile, strippedLine, versions)
     print(f"File {OUTPUT_FILE} created.")
-    out_file.close()
+    outputFile.close()
 
 if __name__ == "__main__":
     main()
