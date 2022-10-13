058a2eed1be275e1a65b902c64bb11f92fe9636b
David Ferreira <david.regatia@gmail.com>
2022-10-13 09:54:51 +0100

    Adds and refactors scripts

---
+++ scripts/setFinalLinksToVersions.py
@@ -0,0 +1,19 @@
+import re
+import os
+from dotenv import load_dotenv
+
+load_dotenv("filePaths.env")
+
+INPUT_FILE = os.getenv('REPOS-LINK-TO-VERSIONS-FILE')
+OUTPUT_FILE = os.getenv('REPOS-LINK-TO-VERSIONS-FINAL-FILE')
+
+inputFile = open(INPUT_FILE, "r")
+outputFile = open(OUTPUT_FILE, "w")
+
+lines = inputFile.readlines()
+inputFile.close()
+for i in lines:
+    patt = re.compile('(\s*)/releases(\s*)')
+    link = patt.sub('\\1\\2', i)
+    outputFile.write(f"https://github.com/{link}")
+outputFile.close()
