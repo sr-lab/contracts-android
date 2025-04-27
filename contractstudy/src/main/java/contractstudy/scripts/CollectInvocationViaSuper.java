7b6e92bd2bf7a13d1b75e18838e0da9ca445cdf8
João F. Ferreira <joao@joaoff.com>
2025-04-27 12:03:01 +0100

    fix(contractstudy): ensure that scs.methodDeclaration is always defined in CollectInvocationViaSuper

--- contractstudy/src/main/java/contractstudy/scripts/CollectInvocationViaSuper.java
+++ contractstudy/src/main/java/contractstudy/scripts/CollectInvocationViaSuper.java
@@ -110,16 +110,18 @@ public class CollectInvocationViaSuper implements Experiment {
     try (PrintWriter out = new PrintWriter(new FileWriter(outputFile))) {
       out.println("program,version,cu,declaration,kind");
       for (SuperCallSite scs : superCallSites) {
-        out.print(scs.programVersion.getName().replaceAll(",", " "));
-        out.print(SEP);
-        out.print(scs.programVersion.getVersion().replaceAll(",", " "));
-        out.print(SEP);
-        out.print(scs.cu);
-        out.print(SEP);
-        out.print(scs.methodDeclaration.replaceAll(",", " "));
-        out.print(SEP);
-        out.print(scs.isMethod ? "method" : "constructor");
-        out.println();
+        if(scs.methodDeclaration != null) {
+          out.print(scs.programVersion.getName().replaceAll(",", " "));
+          out.print(SEP);
+          out.print(scs.programVersion.getVersion().replaceAll(",", " "));
+          out.print(SEP);
+          out.print(scs.cu);
+          out.print(SEP);
+          out.print(scs.methodDeclaration.replaceAll(",", " "));
+          out.print(SEP);
+          out.print(scs.isMethod ? "method" : "constructor");
+          out.println();
+        }
       }
     }
   }
